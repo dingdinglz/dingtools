bbb6b0e352e2a64d9fa777a4ffe70bf2860c327c
丁丁 <dlz1149978856@163.com>
2022-03-04 13:17:47 +0800

    Update version.go

--- version.go
+++ version.go
@@ -1,11 +1,17 @@
 package dingtools
 
-var version string = "v0.1"
+import "fmt"
 
+var version string = "v0.1"
 
 // Version 获取当前版本号
 // 例如：var a = Version()
 // fmt.Println(a)
 func Version() string {
 	return version
+}
+
+// OutVersion 输出版本号
+func OutVersion()  {
+	fmt.Println("dingtools",version)
 }
