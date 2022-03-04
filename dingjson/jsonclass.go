fc9c7473dacf5f0629b8218bacaeb41f0309ab67
丁丁 <97778145+dingdinglz@users.noreply.github.com>
2022-03-04 22:05:34 +0800

    Update jsonclass.go

--- dingjson/jsonclass.go
+++ dingjson/jsonclass.go
@@ -1,6 +1,9 @@
 package dingjson
 
-import "github.com/dingdinglz/dingtools/dinglog"
+import (
+	"encoding/json"
+	"github.com/dingdinglz/dingtools/dinglog"
+)
 
 const (
 	version string = "v0.1"
@@ -14,4 +17,37 @@ func init() {
 // Version 返回dingjson的版本号
 func Version() string {
 	return version
+}
+
+// NewFromStruct 通过struct生成dingjson类型（推荐）
+func NewFromStruct(str interface{}) (*DingJson, error) {
+	marshal, err := json.Marshal(str)
+	if err != nil {
+		return nil, err
+	}
+	return &DingJson{src: marshal}, nil
+}
+
+// NewFromString 通过string生成dingjson类型
+func NewFromString(str string) *DingJson {
+	return &DingJson{src: []byte(str)}
+}
+
+// NewFromBytes 通过[]byte生成dingjson类型
+func NewFromBytes(src []byte) *DingJson {
+	return &DingJson{src: src}
+}
+
+type DingJson struct {
+	src []byte
+}
+
+// ConventToStr 返回string的json
+func (d *DingJson) ConventToStr() string {
+	return string(d.src)
+}
+
+// GetSrc 返回[]byte类型的json
+func (d *DingJson) GetSrc() []byte {
+	return d.src
 }
